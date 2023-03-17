cc2ddca4f37a8c6f4cfcaaadbacfedb975368d73
troias <hunter007nz@gmail.com>
2023-03-17 16:31:06 +1300

    added logo

--- src/components/ui/logo/logo.tsx
+++ src/components/ui/logo/logo.tsx
@@ -1,26 +1,16 @@
-import { useState } from "react";
-import { StarIcon } from "@heroicons/react/24/outline";
+import React from "react";
+import Link from "next/link"; // This is where we'll define our Tailwind classes
 
 const Logo = () => {
-  const [isHovered, setIsHovered] = useState(false);
-
-  const handleHover = () => {
-    setIsHovered(true);
-  };
-
-  const handleLeave = () => {
-    setIsHovered(false);
-  };
-
   return (
-    <div className="flex items-center">
-      <StarIcon
-        className={`h-6 w-6 ml-2 ${
-          isHovered ? "text-yellow-400" : "text-gray-400"
-        }`}
-        onMouseEnter={handleHover}
-        onMouseLeave={handleLeave}
-      />
+    <div className="flex items-center justify-center">
+      <Link href="/">
+        <img
+          src="https://svgshare.com/i/rD6.svg"
+          alt="logo"
+          className="h-5   w-20"
+        />
+      </Link>
     </div>
   );
 };
